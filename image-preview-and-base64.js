c592a00ee808145819d8f40ab652d704c8274399
ai-365@outlook.com <ai-365@outlook.com>
2025-02-17 06:55:49 +0000

    版本02-15-14:55

--- image-preview-and-base64.js
+++ image-preview-and-base64.js
@@ -1,5 +1,5 @@
-
-document.getElementById('imageInput').addEventListener('change', function() {
+const component = document.querySelector('input-component')
+component.shadowRoot.querySelector('#imageInput').addEventListener('change', function() {
     const file = this.files[0];
     if (file) {
         const reader = new FileReader();
@@ -7,8 +7,8 @@ document.getElementById('imageInput').addEventListener('change', function() {
         // 读取文件完成后的回调
         reader.onload = function(e) {
             window.base64 = e.target.result;
-            document.getElementById('imagePreview').src = base64;
-            document.getElementById('imagePreview').style.display = 'inline';
+            component.shadowRoot.querySelector('#imagePreview').src = base64;
+            component.shadowRoot.querySelector('#imagePreview').style.display = 'inline';
         };
 
         // 以DataURL的形式读取文件
---
+++ user-input.js
@@ -0,0 +1,63 @@
+const primaryArea = document.querySelector('.primary')
+// 输入组件：包括输入框、按钮、图片上传、图片预览
+const inputComponent = document.querySelector('input-component')
+// 用户输入框
+const userInput = inputComponent.shadowRoot.querySelector('.user-input')
+// 测试阶段预内容：图中描绘的是什么景象？
+userInput.value = '图中描绘的是什么景象？'
+// 发送按钮节点
+const sendButton = inputComponent.shadowRoot.querySelector('#send-button')
+
+// 页面加载后自动聚焦到输入框：
+window.onload = () => {
+  userInput.focus()
+}
+
+
+// 激活输入框期间，监听Enter键
+userInput.onkeyup = event => {
+  // 检测到Enter键且没按其它控制键，就模拟点击sendButton
+  if (event.key == 'Enter' && !event.ctrlKey && !event.shiftKey && !event.metaKey && userInput.value !== '') {
+    sendButton.click()
+    // 高度复原： 
+    userInput.style.height = '3rem'
+  }
+}
+// 输入框输入时自动调整高度
+userInput.oninput = () => {
+  userInput.style.height = userInput.scrollHeight + 'px';
+}
+
+sendButton.onclick = async function () {
+
+  // 移除输入框和发送按钮的初识样式，回到页面底部
+  inputComponent.style.bottom = '5vh'
+
+  // 清空输入内容
+  // 1、创建问题框，并append到主要问答区
+  // question = questionAvatar（提问者头像） + questionContent（提问内容：重点）
+  const question = document.createElement('div')
+  const questionAvatar = document.createElement('div')
+  const questionContent = document.createElement('div')
+  question.className = 'question'
+  questionAvatar.className = 'question-avatar'
+  questionContent.className = 'question-content'
+  primaryArea.append(question)
+  question.append(questionAvatar)
+  question.append(questionContent)
+
+
+  // 2、将输入框的内容复制到问题库
+  questionContent.textContent = userInput.value
+
+  const answerComponent = document.createElement('answer-component')
+  answerComponent.style.width = '100%'
+  const answerText = answerComponent.shadowRoot.querySelector('#answer-text')
+  primaryArea.append(answerComponent)
+
+
+  // 获取fetchLLM()函数，从云服务中调用大模型
+  const { fetchLLM } = await import('./fetch-llm-picture-input.js')
+  fetchLLM(userInput, answerText)
+
+}
